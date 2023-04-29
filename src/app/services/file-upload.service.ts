b6c62198fd9d03e56b5452af852ea92c3e2b7d3f
arun4006 <aarunkumar334@gmail.com>
2023-04-29 17:04:58 +0530

    angular file

---
+++ src/app/services/file-upload.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpRequest, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FileUploadService {
+  private baseUrl = 'https://d4ujpq8uw6.execute-api.us-east-1.amazonaws.com/dev/file/upload';
+
+  constructor(private http: HttpClient) { }
+
+  upload(file: File): Observable<HttpEvent<any>> {
+    const formData: FormData = new FormData();
+
+    formData.append('file', file);
+
+    const req = new HttpRequest('POST', `${this.baseUrl}`, formData, {
+      reportProgress: true,
+      responseType: 'json'
+    });
+
+    return this.http.request(req);
+  }
+
+  getFiles(): Observable<any> {
+    return this.http.get(`${this.baseUrl}`);
+  }
+}
